b98e36dbb3f19e3328a12bb0d99bafc4de43a849
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-07-25 21:29:06 +0530

    Create Regular_Expression_matching.py

---
+++ Regular_Expression_matching.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        
+        if len(p) == 1 or p[1] != '*':
+            if len(s) > 0 and (p[0] == s[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
+        else:
+            while len(s) > 0 and (p[0] == s[0] or p[0] == '.'):
+                if self.isMatch(s, p[2:]):
+                    return True
+                s = s[1:]
+            return self.isMatch(s, p[2:])
