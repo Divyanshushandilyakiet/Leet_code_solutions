0db5b093f1d392f73cb6ac4fa8c57fc6c68730e0
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-06 22:53:35 +0530

    Create Flipping_an_image.cpp

---
+++ Flipping_an_image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) 
+    {
+        int n=image.size();
+        for(int i=0;i<n;i++){
+            reverse(image[i].begin(),image[i].end());
+            for(int j=0;j<n;j++)
+            {
+                image[i][j] ^=1;
+            }
+        }
+        return image;
+    }
+};
