d93d65701c49cc1197a5b790c6b29788d2bfc439
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-08-14 23:29:22 +0530

    Create Single Number III.cpp

---
+++ Single Number III.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> res;
+       sort(nums.begin() ,nums.end());
+        for(int i=0;i<nums.size();i++){ 
+            if (count(nums.begin(), nums.end(),nums[i])==1)
+               res.push_back(nums[i]);
+        }
+        return res;
+    }
+};
